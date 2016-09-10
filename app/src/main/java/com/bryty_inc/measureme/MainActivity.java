55e0e64ecf0ac9824c8efcaf140bee0ffd9279de
tyemerick <emerickt@purdue.edu>
2016-09-09 22:55:35 -0400

    Main Activity - Added button function activity_main.xml - Added a button and a txt view
    MeasureActivity :
     - Added instrctions
     - Added Touch Event Listener
     - Added Count Down Timer
    
     activity_measure - Add 2 Number based text views and instructions

--- app/src/main/java/com/bryty_inc/measureme/MainActivity.java
+++ app/src/main/java/com/bryty_inc/measureme/MainActivity.java
@@ -1,13 +1,28 @@
 package com.bryty_inc.measureme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.button_start);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeasureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
